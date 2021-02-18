0e6c05a92e4dbcdd758030b466235e058f71f4a8
E. Hannah <echannah631@gmail.com>
2021-02-18 07:05:26 -0800

    text field implementation, light/dark mode adaptations

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Text field should report back to view controller
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    //Delegate method when the Go button is pressed, setup by the .delegate property in viewDidLoad
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    //Delegate method that's triggered when .endEditing equals true
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use the searchTextFiled.text to get the weather for that city
+        print(textField.text!)
+        
+        textField.text = ""
+    }
+    
 }
 
